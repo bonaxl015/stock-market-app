a9854112c357b6f868ed924333d9f2c82d620945
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-14 01:14:59 +0800

    Changes root path to home/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
