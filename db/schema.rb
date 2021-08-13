afc9b6ea3cf2bbd3bc833bcd32856b0c4a0e5957
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-13 20:14:43 +0800

    Migrate user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_105112) do
+ActiveRecord::Schema.define(version: 2021_08_13_115527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,9 @@ ActiveRecord::Schema.define(version: 2021_08_13_105112) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "username"
-    t.boolean "admin", default: true
-    t.boolean "broker", default: false
-    t.boolean "buyer", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
