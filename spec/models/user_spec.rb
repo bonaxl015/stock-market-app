53c46cb6574f9784818a634e11000b9a07478da2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-14 10:09:19 +0800

    Update test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject do
+  subject(:user) do
     described_class.create(email: 'example@mail.com',
                            password: 'password',
                            username: 'Username01',
@@ -10,58 +10,58 @@ RSpec.describe User, type: :model do
 
   context 'with valid attributes' do
     it 'does validate' do
-      expect(subject).to be_valid
+      expect(user).to be_valid
     end
   end
 
   context 'without a username' do
     before do
-      subject.username = nil
+      user.username = nil
     end
 
     it 'does not validate' do
-      expect(subject).to_not be_valid
+      expect(user).not_to be_valid
     end
   end
 
   context 'when username is not unique' do
     before do
-      User.create(email: 'example2@mail.com',
-                  password: 'password2',
-                  username: 'Username01',
-                  user_type: 'Buyer')
+      described_class.create(email: 'example2@mail.com',
+                             password: 'password2',
+                             username: 'Username01',
+                             user_type: 'Buyer')
     end
 
     it 'does not validate' do
-      subject.username = subject.username
-      expect(subject).to_not be_valid
+      user.username = user.username
+      expect(user).not_to be_valid
     end
 
     context 'when not case sensitive' do
       it 'does not validate' do
-        subject.username = subject.username.downcase
-        expect(subject).to_not be_valid
+        user.username = user.username.downcase
+        expect(user).not_to be_valid
       end
     end
   end
 
   context 'when username is less than 6 characters' do
     before do
-      subject.username = 'A' * 5
+      user.username = 'A' * 5
     end
 
     it 'does not validate' do
-      expect(subject).to_not be_valid
+      expect(user).not_to be_valid
     end
   end
 
   context 'when username contains special characters' do
     before do
-      subject.username = 'Username.01'
+      user.username = 'Username.01'
     end
 
     it 'does not validate' do
-      expect(subject).to_not be_valid
+      expect(user).not_to be_valid
     end
   end
 end
