5f9c9c25fecf20de835ade9c0ff64cd457b4e339
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-15 01:46:57 +0800

    Added index and market methods

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,32 @@
 class StocksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user_stocks = Stock.where(user_id: current_user.id)
+  end
+
+  def show
+    
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def market
+    @stocks = Stock.all
+  end
+
+  private
+
+  def stock_params
+  end
 end
