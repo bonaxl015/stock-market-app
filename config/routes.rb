f28c8251a3515c8eb64aaae9e192bee1216e97e9
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-15 01:53:47 +0800

    Added resources for stocks and orders, added stocks/market path and added routing to registration_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  devise_for :users
+  root to: 'home#index'
+  get 'home/index'
+  get 'stocks/market'
+  devise_for :users, controllers: { registrations: 'registrations' }
+  resources :stocks do
+    resources :orders
+  end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
