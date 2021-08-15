b0f3ea68b08c1a604bb70dd106ced6b7a3db8d9a
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-15 20:06:39 +0800

    Update stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,10 @@ class Stock < ApplicationRecord
   has_many :orders, dependent: :destroy
   belongs_to :user
 
-  validates :name, presence: true
-  validates :unit_price, presence: true
-  validates :shares, presence: true
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false }
+  validates :unit_price, presence: true,
+                         numericality: { greater_than: 0 }
+  validates :shares, presence: true,
+                     numericality: { greater_than: 0 }
 end
