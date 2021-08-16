73cbef8fed98c31eb5f54bfcce7d5bf88f20719d
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-16 23:55:26 +0800

    Fixed formatting

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,18 +1,14 @@
 module OrdersHelper
 
   def process_buy_stock(order_shares, stock_id)
-    @stock_for_update = Stock.find_by(id: stock_id)
-    @stock_for_update.shares -= order_shares
-    @stock_for_update.save
+    stock_for_update = Stock.find_by(id: stock_id)
+    stock_for_update.shares -= order_shares
+    stock_for_update.save
   end
 
   def process_sell_stock(order_shares, stock_id)
-    @stock_for_update = Stock.find_by(id: stock_id)
-    @stock_for_update.shares += order_shares
-    @stock_for_update.save
-  end
-
-  def compare_shares(current_shares, new_shares)
-    return current_shares == new_shares
+    stock_for_update = Stock.find_by(id: stock_id)
+    stock_for_update.shares += order_shares
+    stock_for_update.save
   end
 end
