434c924b26b9416c039d615a305989ea4524592a
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-16 18:14:25 +0800

    Update rails admin config

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,13 +3,15 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
+  config.parent_controller = 'ApplicationController'
 
   ## == CancanCan ==
-  # config.authorize_with :cancancan
+  config.authorize_with :cancancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
