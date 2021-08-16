ca6598088bbc66a02641ef9a2751ed3bc38ca25a
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-16 23:38:01 +0800

    Added helper methods for buying and selling stocks

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,18 @@
 module OrdersHelper
+
+  def process_buy_stock(order_shares, stock_id)
+    @stock_for_update = Stock.find_by(id: stock_id)
+    @stock_for_update.shares -= order_shares
+    @stock_for_update.save
+  end
+
+  def process_sell_stock(order_shares, stock_id)
+    @stock_for_update = Stock.find_by(id: stock_id)
+    @stock_for_update.shares += order_shares
+    @stock_for_update.save
+  end
+
+  def compare_shares(current_shares, new_shares)
+    return current_shares == new_shares
+  end
 end
