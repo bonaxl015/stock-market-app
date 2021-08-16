411316762a52df54eb33190671fbdd21c191c2d1
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-16 23:35:58 +0800

    Removed unnecessary routes and added orders/all route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/index'
   get 'stocks/market'
+  get 'orders/all'
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :stocks do
-    resources :orders
+  resources :stocks, except: [:show] do
+    resources :orders, only: [:new, :create, :edit, :update]
   end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
