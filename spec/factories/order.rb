954d41448b5680af2a97ebbe8d31e7653d6944f9
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-17 16:57:28 +0800

    Updated order factory

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -1,9 +1,25 @@
 FactoryBot.define do
   factory :order do
-    name { Faker::Company.unique.name }
+    name { Faker::Company.name }
     unit_price { Faker::Number.number(digits: 5) }
     shares { Faker::Number.number(digits: 6) }
     association :user
     association :stock
+
+    trait :invalid_attributes do
+      name { nil }
+      unit_price { nil }
+      shares { nil }
+      association :user
+      association :stock
+    end
+
+    trait :new_attributes do
+      name { Faker::Company.name }
+      unit_price { Faker::Number.number(digits: 4) }
+      shares { Faker::Number.number(digits: 5) }
+      association :user
+      association :stock
+    end
   end
 end
