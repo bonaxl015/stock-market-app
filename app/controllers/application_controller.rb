08d52dce4aac79b4e53d4452b6e7ed4f4f62a657
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-17 17:24:47 +0800

    Added condition on routing after sign in based on user type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
-    stocks_market_path
+    current_user.user_type == 'Admin' ? rails_admin_path : stocks_market_path
   end
 
   def configure_permitted_parameters
