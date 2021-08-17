9102e592a4cf96d601b419b075d3bba419015013
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-17 15:18:37 +0800

    Migrate table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_132736) do
+ActiveRecord::Schema.define(version: 2021_08_17_071433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2021_08_15_132736) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "stock_id", null: false
     t.bigint "user_id", null: false
-    t.index ["name"], name: "index_orders_on_name", unique: true
     t.index ["stock_id"], name: "index_orders_on_stock_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
