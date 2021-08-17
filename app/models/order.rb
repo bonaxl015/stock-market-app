0edc123f39d84d6810b942b7edd019ad30189563
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-17 15:21:10 +0800

    Remove uniqueness validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,7 @@ class Order < ApplicationRecord
   belongs_to :stock
   belongs_to :user
 
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: false }
+  validates :name, presence: true
   validates :unit_price, presence: true,
                          numericality: { greater_than: 0 }
   validates :shares, presence: true,
