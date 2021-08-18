a9ad8c7a8003f7256551ec0a6921f3039b3436ed
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-18 21:01:08 +0800

    Fixed formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
-
   def devise_controllers?(current_controller)
-    %w(sessions registrations passwords).any? { |controller| controller.include? current_controller }
+    %w[sessions registrations passwords].any? { |controller| controller.include? current_controller }
   end
 end
