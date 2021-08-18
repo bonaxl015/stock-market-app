27b1f68c6ad6cdb5984183d031c2476573b1cd78
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-18 23:28:04 +0800

    Update signing in users test

--- spec/system/signing_in_users_spec.rb
+++ spec/system/signing_in_users_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "SigningInUsers", type: :system do
-  let(:create_broker_user) { create(:user, user_type: 'Broker') }
-  let(:create_buyer_user) { create(:user, user_type: 'Buyer') }
-
   shared_examples 'signs in user' do
     it 'signs in user' do
       expect(page).to have_content('Signed in successfully')
@@ -14,7 +11,29 @@ RSpec.describe "SigningInUsers", type: :system do
     driven_by(:rack_test)
   end
 
-  context 'when user signing in is a broker' do
+  context 'when user signing in is admin' do
+    let(:create_admin_user) { create(:user, user_type: 'Admin') }
+
+    before do
+      create_admin_user
+      visit root_path
+      find('a[href="/users/sign_in"]').click
+
+      fill_in 'user[email]', with: create_admin_user.email
+      fill_in 'user[password]', with: create_admin_user.password
+      find('input[type="submit"]').click
+    end
+
+    include_examples 'signs in user'
+
+    it 'renders page with admin dashboard' do
+      expect(page).to have_content('Site Administration')
+    end
+  end
+
+  context 'when user signing in is not admin (broker)' do
+    let(:create_broker_user) { create(:user, user_type: 'Broker') }
+
     before do
       create_broker_user
       visit root_path
@@ -32,7 +51,9 @@ RSpec.describe "SigningInUsers", type: :system do
     end
   end
 
-  context 'when user signing in is not a broker (buyer)' do
+  context 'when user signing in is not admin (buyer)' do
+    let(:create_buyer_user) { create(:user, user_type: 'Buyer') }
+
     before do
       create_buyer_user
       visit root_path
