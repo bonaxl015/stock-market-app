a35a773776075fdc5c463174c6025448509bbb7b
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-18 20:47:03 +0800

    Added helper method to check if the controller is in devise controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def has_devise_controllers(current_controller)
+    ['sessions', 'registrations', 'passwords'].any? { |controller| controller.include? current_controller }
+  end
 end
