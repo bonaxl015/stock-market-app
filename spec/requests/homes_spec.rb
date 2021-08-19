a053c28815d35dfc8e88eaf493a23809e9302991
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-19 11:36:15 +0800

    Update examples

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  shared_examples 'raises an error' do
-    it 'raises an error' do
-      expect do
-        get '/home/new'
-      end.to raise_error(ActionController::RoutingError)
-    end
-  end
-
   describe 'GET /index' do
     before do
       get home_index_path
@@ -20,10 +12,18 @@ RSpec.describe 'Homes', type: :request do
   end
 
   describe 'GET /show' do
-    include_examples 'raises an error'
+    it 'raises an error' do
+      expect do
+        get '/home/1'
+      end.to raise_error(ActionController::RoutingError)
+    end
   end
 
   describe 'GET /new' do
-    include_examples 'raises an error'
+    it 'raises an error' do
+      expect do
+        get '/home/new'
+      end.to raise_error(ActionController::RoutingError)
+    end
   end
 end
