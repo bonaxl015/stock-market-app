38c1ca624e52ac4554e7bba3f35cadbcf5db7e26
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-26 16:02:38 +0800

    Added confirmable helpers and added method to set user status upon registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 6 },
                        format: { with: /\A[A-Za-z0-9]+\z/ }
   has_many :stocks, dependent: :destroy
   has_many :orders, dependent: :destroy
+  before_save :require_confirmation
+
+  def require_confirmation
+    self.approved = user_type == 'Broker' ? false : true
+    self.skip_confirmation! if self.approved == true
+  end
 end
