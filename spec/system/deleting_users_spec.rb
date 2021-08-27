a33bf8c66ec875a57f2a5086c43fd01e30e9e851
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-27 22:16:41 +0800

    Update deleting users test

--- spec/system/deleting_users_spec.rb
+++ spec/system/deleting_users_spec.rb
@@ -1,35 +1,35 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'DeletingUsers', type: :system do
-#   before do
-#     driven_by(:rack_test)
-#   end
+RSpec.describe 'DeletingUsers', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
 
-#   context 'when user signed in is admin' do
-#     let(:create_user) { create(:user, user_type: 'Broker') }
+  context 'when user signed in is admin' do
+    let(:create_user) { create(:user, user_type: 'Broker') }
 
-#     before do
-#       sign_in create(:user, user_type: 'Admin')
-#       create_user
+    before do
+      sign_in create(:user, user_type: 'Admin')
+      create_user
 
-#       visit rails_admin.index_path('user')
-#       find("a[href*='user/#{create_user.id}/delete']").click
-#       click_on "Yes, I'm sure"
-#     end
+      visit rails_admin.index_path('user')
+      find("a[href*='user/#{create_user.id}/delete']").click
+      click_on "Yes, I'm sure"
+    end
 
-#     it 'deletes a user' do
-#       expect(User.find_by(id: create_user.id)).to eq(nil)
-#     end
-#   end
+    it 'deletes a user' do
+      expect(User.find_by(id: create_user.id)).to eq(nil)
+    end
+  end
 
-#   context 'when user signed in is not admin' do
-#     before do
-#       sign_in create(:user)
-#       visit rails_admin_path
-#     end
+  context 'when user signed in is not admin' do
+    before do
+      sign_in create(:user)
+      visit rails_admin_path
+    end
 
-#     it 'unauthorizes access' do
-#       expect(page).to have_content('not authorized')
-#     end
-#   end
-# end
+    it 'unauthorizes access' do
+      expect(page).to have_content('not authorized')
+    end
+  end
+end
