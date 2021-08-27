e93913b55e3b3fcbf5e7a181cde92ef0d5f80c79
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-28 00:48:53 +0800

    Add user mailer test

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  let(:user) { create(:user) }
+  let(:mail) { described_class.success_notification(user).deliver_now }
+
+  it 'renders the subject' do
+    expect(mail.subject).to eq('Stock Market App Registration Successful!')
+  end
+
+  it 'renders the receiver email' do
+    expect(mail.to).to eq([user.email])
+  end
+
+  it 'renders the sender email' do
+    expect(mail.from).to eq(['from@example.com'])
+  end
+
+  it 'assigns @username' do
+    expect(mail.body.encoded).to match(user.username)
+  end
+end
