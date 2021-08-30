d228fcfe1503535d1d4629f61ac4642e95bec955
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-30 10:04:09 +0800

    Changed default url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'bon-patrick-stock-market.herokuapp.com/', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
