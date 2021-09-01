03066e5ae2d8b8dd443a1597cf7641a20754d95c
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-01 21:21:55 +0800

    Add validation for shares connected to money

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,16 @@ class Order < ApplicationRecord
                          numericality: { greater_than: 0 }
   validates :shares, presence: true,
                      numericality: { greater_than: 0 }
+  validate :check_money
+
+  def check_money
+    unless self.shares.nil? || self.unit_price.nil?
+      order_price = self.shares * self.unit_price
+      buyer_money = self.user.money
+
+      if order_price > buyer_money
+        errors.add(:shares, 'You cannot afford that number of shares.')
+      end
+    end
+  end
 end
