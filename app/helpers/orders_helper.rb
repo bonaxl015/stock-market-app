36262c25f617372006de02d64245159149a1ab60
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-02 18:12:48 +0800

    Fixed selling stock method, applied proper variable naming for buying stock method and created method for calculating total investments of the user

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,31 +1,41 @@
 module OrdersHelper
-  def process_buy_stock(current_user_id, order_shares, stock_id)
+  def process_buy_stock(user_id, order_shares, stock_id)
     stock = Stock.find_by(id: stock_id)
+    buyer = User.find_by(id: user_id)
+
     stock.shares -= order_shares
     stock.save
-
-    buyer = User.find_by(id: current_user_id)
     total_price = stock.unit_price * order_shares
     buyer.money -= total_price
     buyer.save
   end
 
-  def process_sell_stock(stock_id, current_user_id, order_id, current_shares)
+  def process_sell_stock(stock_id, user_id, order_id, current_shares)
     order = Order.find_by(id: order_id)
     stock = Stock.find_by(id: stock_id)
+    buyer = User.find_by(id: user_id)
+
     stock.shares += order.shares
     stock.save
-
     if current_shares == order.shares
+      total_price = stock.unit_price * order.shares
       order.delete
     else
+      total_price = stock.unit_price * order.shares
       order.shares = current_shares - order.shares
       order.save
     end
-
-    buyer = User.find_by(id: current_user_id)
-    total_price = stock.unit_price * current_shares
     buyer.money += total_price
     buyer.save
+  end
+
+  def calculate_investments(user_orders)
+    total_investments = 0
+    unless user_orders.count == 0
+      user_orders.each do |order|
+        total_investments += order.shares * order.unit_price
+      end
+    end
+    total_investments.round(2)
   end
 end
