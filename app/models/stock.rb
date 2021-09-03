fca68ed8b9784228dbd9f4f19f8a7bfc2cb23cf3
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-03 21:09:50 +0800

    Fixed rubocop error

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,7 +10,7 @@ class Stock < ApplicationRecord
                      numericality: { greater_than: 0 }
 
   def self.iex_api
-    client = IEX::Api::Client.new(
+    IEX::Api::Client.new(
       publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
       secret_token: ENV['IEX_API_SECRET_TOKEN'],
       endpoint: 'https://cloud.iexapis.com/v1'
