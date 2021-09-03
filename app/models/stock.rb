7ce093fa3d9e2720c13b3657ddeb4360f43c8a07
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-03 21:07:06 +0800

    Added IEX configs

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,4 +8,12 @@ class Stock < ApplicationRecord
                          numericality: { greater_than: 0 }
   validates :shares, presence: true,
                      numericality: { greater_than: 0 }
+
+  def self.iex_api
+    client = IEX::Api::Client.new(
+      publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
+      secret_token: ENV['IEX_API_SECRET_TOKEN'],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+  end
 end
