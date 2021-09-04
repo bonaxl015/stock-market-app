c223b574222304cf0cbe4434396389c62350ac91
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-04 18:35:44 +0800

    Update creating orders test

--- spec/system/creating_orders_spec.rb
+++ spec/system/creating_orders_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'CreatingOrders', type: :system do
-  let(:create_user) { create(:user, user_type: 'Buyer') }
-  let(:create_stock) { create(:stock) }
+  let(:create_buyer) { create(:user, user_type: 'Buyer') }
+  let(:create_broker) { create(:user, user_type: 'Broker') }
+  let(:create_stock) { create(:stock, user_id: create_broker.id) }
   let(:buy_button) { "a[href='/stocks/#{create_stock.id}/orders/new']" }
 
   before do
@@ -11,13 +12,10 @@ RSpec.describe 'CreatingOrders', type: :system do
 
   context 'when user signed in is buyer' do
     let(:order_shares) { Faker::Number.number(digits: 2) }
-
-    let(:remaining_money) do
-      create_user.money - (Order.find_by(stock_id: create_stock.id).unit_price * order_shares)
-    end
+    let(:total_price) { (Order.find_by(stock_id: create_stock.id).unit_price * order_shares) }
 
     before do
-      sign_in create_user
+      sign_in create_buyer
       create_stock
       visit stocks_market_path
       find(buy_button).click
@@ -34,8 +32,12 @@ RSpec.describe 'CreatingOrders', type: :system do
       expect(Stock.find_by(id: create_stock.id).shares).to eq(create_stock.shares - order_shares)
     end
 
-    it 'decrements user money' do
-      expect(User.find_by(id: create_user.id).money).to eq(remaining_money)
+    it "decrements buyer's money" do
+      expect(User.find_by(id: create_buyer.id).money).to eq(create_buyer.money - total_price)
+    end
+
+    it "increments broker's money" do
+      expect(User.find_by(id: create_broker.id).money).to eq(create_broker.money + total_price)
     end
   end
 
