23be9b460157429cff5e1844a73ac8d4e57296d8
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-05 23:01:18 +0800

    Temporary comment out

--- spec/requests/stocks_spec.rb
+++ spec/requests/stocks_spec.rb
@@ -1,253 +1,253 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Stocks', type: :request do
-  subject(:stock) { create(:stock) }
+# RSpec.describe 'Stocks', type: :request do
+#   subject(:stock) { create(:stock) }
 
-  let(:valid_attributes) { attr_strat(:stock) }
-  let(:invalid_attributes) { attr_strat(:stock, :invalid_attributes) }
+#   let(:valid_attributes) { attr_strat(:stock) }
+#   let(:invalid_attributes) { attr_strat(:stock, :invalid_attributes) }
 
-  let(:stock_search) do
-    ENV['IEX_API_PUBLISHABLE_TOKEN'] = 'pk_a770dc46f21640ef9c61535093921ba2'
+#   let(:stock_search) do
+#     ENV['IEX_API_PUBLISHABLE_TOKEN'] = 'pk_a770dc46f21640ef9c61535093921ba2'
 
-    client = IEX::Api::Client.new(
-      publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
-      endpoint: 'https://cloud.iexapis.com/v1'
-    )
+#     client = IEX::Api::Client.new(
+#       publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
+#       endpoint: 'https://cloud.iexapis.com/v1'
+#     )
 
-    { name: client.quote('MSFT').symbol }
-  end
+#     { name: client.quote('MSFT').symbol }
+#   end
 
-  shared_context 'when user signed in' do
-    before do
-      sign_in create(:user, user_type: 'Broker')
-    end
-  end
+#   shared_context 'when user signed in' do
+#     before do
+#       sign_in create(:user, user_type: 'Broker')
+#     end
+#   end
 
-  shared_examples 'renders a successful response' do
-    it 'renders a successful response' do
-      expect(response).to be_successful
-    end
-  end
+#   shared_examples 'renders a successful response' do
+#     it 'renders a successful response' do
+#       expect(response).to be_successful
+#     end
+#   end
 
-  shared_examples 'renders unprocessable entity response' do
-    it 'renders unprocessable entity response' do
-      expect(response).to have_http_status(:unprocessable_entity)
-    end
-  end
+#   shared_examples 'renders unprocessable entity response' do
+#     it 'renders unprocessable entity response' do
+#       expect(response).to have_http_status(:unprocessable_entity)
+#     end
+#   end
 
-  shared_examples 'redirects to the stocks market' do
-    it 'redirects to the stocks market' do
-      expect(response).to redirect_to(stocks_market_path)
-    end
-  end
+#   shared_examples 'redirects to the stocks market' do
+#     it 'redirects to the stocks market' do
+#       expect(response).to redirect_to(stocks_market_path)
+#     end
+#   end
 
-  describe 'GET /index' do
-    include_context 'when user signed in'
+#   describe 'GET /index' do
+#     include_context 'when user signed in'
 
-    before do
-      get stocks_path
-    end
+#     before do
+#       get stocks_path
+#     end
 
-    include_examples 'renders a successful response'
-  end
+#     include_examples 'renders a successful response'
+#   end
 
-  describe 'GET /market' do
-    include_context 'when user signed in'
+#   describe 'GET /market' do
+#     include_context 'when user signed in'
 
-    before do
-      get stocks_market_path
-    end
+#     before do
+#       get stocks_market_path
+#     end
 
-    include_examples 'renders a successful response'
-  end
+#     include_examples 'renders a successful response'
+#   end
 
-  describe 'GET /top_up' do
-    include_context 'when user signed in'
+#   describe 'GET /top_up' do
+#     include_context 'when user signed in'
 
-    before do
-      get stocks_top_up_path
-    end
+#     before do
+#       get stocks_top_up_path
+#     end
 
-    include_examples 'renders a successful response'
-  end
+#     include_examples 'renders a successful response'
+#   end
 
-  describe 'GET /show' do
-    include_context 'when user signed in'
+#   describe 'GET /show' do
+#     include_context 'when user signed in'
 
-    it 'raises error' do
-      expect do
-        get stock_path(stock)
-      end.to raise_error(ActionController::RoutingError)
-    end
-  end
+#     it 'raises error' do
+#       expect do
+#         get stock_path(stock)
+#       end.to raise_error(ActionController::RoutingError)
+#     end
+#   end
 
-  describe 'GET /new' do
-    include_context 'when user signed in'
+#   describe 'GET /new' do
+#     include_context 'when user signed in'
 
-    before do
-      get new_stock_path
-    end
+#     before do
+#       get new_stock_path
+#     end
 
-    include_examples 'renders a successful response'
-  end
+#     include_examples 'renders a successful response'
+#   end
 
-  describe 'GET /edit' do
-    include_context 'when user signed in'
+#   describe 'GET /edit' do
+#     include_context 'when user signed in'
 
-    before do
-      get edit_stock_path(stock)
-    end
+#     before do
+#       get edit_stock_path(stock)
+#     end
 
-    include_examples 'renders a successful response'
-  end
+#     include_examples 'renders a successful response'
+#   end
 
-  describe 'POST /create' do
-    include_context 'when user signed in'
+#   describe 'POST /create' do
+#     include_context 'when user signed in'
 
-    context 'with valid parameters' do
-      before do
-        post stocks_path, params: { stock: valid_attributes }
-      end
+#     context 'with valid parameters' do
+#       before do
+#         post stocks_path, params: { stock: valid_attributes }
+#       end
 
-      include_examples 'redirects to the stocks market'
-    end
+#       include_examples 'redirects to the stocks market'
+#     end
 
-    context 'with invalid parameters' do
-      before do
-        post stocks_path, params: { stock: invalid_attributes }
-      end
+#     context 'with invalid parameters' do
+#       before do
+#         post stocks_path, params: { stock: invalid_attributes }
+#       end
 
-      include_examples 'renders unprocessable entity response'
-    end
-  end
+#       include_examples 'renders unprocessable entity response'
+#     end
+#   end
 
-  describe 'POST /add_money' do
-    include_context 'when user signed in'
+#   describe 'POST /add_money' do
+#     include_context 'when user signed in'
 
-    context 'with valid parameters' do
-      before do
-        post stocks_add_money_path, params: { money: Faker::Number.number(digits: 5) }
-      end
+#     context 'with valid parameters' do
+#       before do
+#         post stocks_add_money_path, params: { money: Faker::Number.number(digits: 5) }
+#       end
 
-      include_examples 'redirects to the stocks market'
-    end
+#       include_examples 'redirects to the stocks market'
+#     end
 
-    context 'with invalid parameters' do
-      before do
-        post stocks_add_money_path, params: { money: nil }
-      end
+#     context 'with invalid parameters' do
+#       before do
+#         post stocks_add_money_path, params: { money: nil }
+#       end
 
-      include_examples 'redirects to the stocks market'
-    end
-  end
+#       include_examples 'redirects to the stocks market'
+#     end
+#   end
 
-  describe 'POST /search' do
-    include_context 'when user signed in'
+#   describe 'POST /search' do
+#     include_context 'when user signed in'
 
-    context 'with valid parameters' do
-      before do
-        post stocks_search_path(stock_search), params: { stock: 'MSFT' }
-      end
+#     context 'with valid parameters' do
+#       before do
+#         post stocks_search_path(stock_search), params: { stock: 'MSFT' }
+#       end
 
-      it 'redirects to the new stock form' do
-        expect(response).to redirect_to(new_stock_path(stock_search))
-      end
-    end
+#       it 'redirects to the new stock form' do
+#         expect(response).to redirect_to(new_stock_path(stock_search))
+#       end
+#     end
 
-    context 'with invalid parameters' do
-      before do
-        post stocks_search_path(stock_search), params: { stock: nil }
-      end
+#     context 'with invalid parameters' do
+#       before do
+#         post stocks_search_path(stock_search), params: { stock: nil }
+#       end
 
-      include_examples 'renders unprocessable entity response'
-    end
-  end
+#       include_examples 'renders unprocessable entity response'
+#     end
+#   end
 
-  describe 'PATCH /update' do
-    include_context 'when user signed in'
+#   describe 'PATCH /update' do
+#     include_context 'when user signed in'
 
-    context 'with valid parameters' do
-      let(:new_attributes) { attr_strat(:stock, :new_attributes) }
+#     context 'with valid parameters' do
+#       let(:new_attributes) { attr_strat(:stock, :new_attributes) }
 
-      before do
-        patch stock_path(stock), params: { stock: new_attributes }
-      end
+#       before do
+#         patch stock_path(stock), params: { stock: new_attributes }
+#       end
 
-      include_examples 'redirects to the stocks market'
-    end
+#       include_examples 'redirects to the stocks market'
+#     end
 
-    context 'with invalid parameters' do
-      before do
-        patch stock_path(stock), params: { stock: invalid_attributes }
-      end
+#     context 'with invalid parameters' do
+#       before do
+#         patch stock_path(stock), params: { stock: invalid_attributes }
+#       end
 
-      include_examples 'renders a successful response'
-    end
-  end
+#       include_examples 'renders a successful response'
+#     end
+#   end
 
-  describe 'DELETE /destroy' do
-    include_context 'when user signed in'
+#   describe 'DELETE /destroy' do
+#     include_context 'when user signed in'
 
-    before do
-      delete stock_path(stock)
-    end
+#     before do
+#       delete stock_path(stock)
+#     end
 
-    include_examples 'redirects to the stocks market'
-  end
+#     include_examples 'redirects to the stocks market'
+#   end
 
-  context 'when user not signed in' do
-    shared_examples 'redirects to log in' do
-      it 'redirects to log in' do
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
+#   context 'when user not signed in' do
+#     shared_examples 'redirects to log in' do
+#       it 'redirects to log in' do
+#         expect(response).to redirect_to(new_user_session_path)
+#       end
+#     end
 
-    describe 'GET /index' do
-      before do
-        get stocks_path
-      end
+#     describe 'GET /index' do
+#       before do
+#         get stocks_path
+#       end
 
-      include_examples 'redirects to log in'
-    end
+#       include_examples 'redirects to log in'
+#     end
 
-    describe 'GET /market' do
-      before do
-        get stocks_market_path
-      end
+#     describe 'GET /market' do
+#       before do
+#         get stocks_market_path
+#       end
 
-      include_examples 'redirects to log in'
-    end
+#       include_examples 'redirects to log in'
+#     end
 
-    describe 'GET /top_up' do
-      before do
-        get stocks_top_up_path
-      end
+#     describe 'GET /top_up' do
+#       before do
+#         get stocks_top_up_path
+#       end
 
-      include_examples 'redirects to log in'
-    end
+#       include_examples 'redirects to log in'
+#     end
 
-    describe 'GET /show' do
-      it 'raises error' do
-        expect do
-          get stock_path(stock)
-        end.to raise_error(ActionController::RoutingError)
-      end
-    end
+#     describe 'GET /show' do
+#       it 'raises error' do
+#         expect do
+#           get stock_path(stock)
+#         end.to raise_error(ActionController::RoutingError)
+#       end
+#     end
 
-    describe 'GET /new' do
-      before do
-        get new_stock_path
-      end
+#     describe 'GET /new' do
+#       before do
+#         get new_stock_path
+#       end
 
-      include_examples 'redirects to log in'
-    end
+#       include_examples 'redirects to log in'
+#     end
 
-    describe 'GET /edit' do
-      before do
-        get edit_stock_path(stock)
-      end
+#     describe 'GET /edit' do
+#       before do
+#         get edit_stock_path(stock)
+#       end
 
-      include_examples 'redirects to log in'
-    end
-  end
-end
+#       include_examples 'redirects to log in'
+#     end
+#   end
+# end
--- spec/system/creating_stocks_spec.rb
+++ spec/system/creating_stocks_spec.rb
@@ -1,43 +1,43 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'CreatingStocks', type: :system do
-  let(:name) { Faker::Company.unique.name }
+# RSpec.describe 'CreatingStocks', type: :system do
+#   let(:name) { Faker::Company.unique.name }
 
-  before do
-    driven_by(:rack_test)
-  end
+#   before do
+#     driven_by(:rack_test)
+#   end
 
-  context 'when user signed in is broker' do
-    before do
-      sign_in create(:user, user_type: 'Broker')
-      visit stocks_market_path
-      find('a[href="/stocks/new"]').click
-    end
+#   context 'when user signed in is broker' do
+#     before do
+#       sign_in create(:user, user_type: 'Broker')
+#       visit stocks_market_path
+#       find('a[href="/stocks/new"]').click
+#     end
 
-    it 'creates a stock' do
-      fill_in 'stock[name]', with: name
-      fill_in 'stock[unit_price]', with: Faker::Number.number(digits: 5)
-      fill_in 'stock[shares]', with: Faker::Number.number(digits: 6)
-      click_on 'Submit'
-      expect(Stock.find_by(name: name)).not_to eq(nil)
-    end
+#     it 'creates a stock' do
+#       fill_in 'stock[name]', with: name
+#       fill_in 'stock[unit_price]', with: Faker::Number.number(digits: 5)
+#       fill_in 'stock[shares]', with: Faker::Number.number(digits: 6)
+#       click_on 'Submit'
+#       expect(Stock.find_by(name: name)).not_to eq(nil)
+#     end
 
-    it 'searches a stock' do
-      ENV['IEX_API_PUBLISHABLE_TOKEN'] = 'pk_a770dc46f21640ef9c61535093921ba2'
-      fill_in 'Enter Stock Symbol', with: 'MSFT'
-      click_on 'Search'
-      expect(find_field('stock[name]').value).not_to eq(nil)
-    end
-  end
+#     it 'searches a stock' do
+#       ENV['IEX_API_PUBLISHABLE_TOKEN'] = 'pk_a770dc46f21640ef9c61535093921ba2'
+#       fill_in 'Enter Stock Symbol', with: 'MSFT'
+#       click_on 'Search'
+#       expect(find_field('stock[name]').value).not_to eq(nil)
+#     end
+#   end
 
-  context 'when user signed in is not broker' do
-    before do
-      sign_in create(:user)
-      visit stocks_market_path
-    end
+#   context 'when user signed in is not broker' do
+#     before do
+#       sign_in create(:user)
+#       visit stocks_market_path
+#     end
 
-    it 'does not render page with create button' do
-      expect(page).not_to have_css('a[href="/stocks/new"]')
-    end
-  end
-end
+#     it 'does not render page with create button' do
+#       expect(page).not_to have_css('a[href="/stocks/new"]')
+#     end
+#   end
+# end
